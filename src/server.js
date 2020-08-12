de589a42fcc0fccdf6b89859c735845673891ab6
Danilo D Vaz <danilodvaz@gmail.com>
2020-08-12 09:18:20 -0300

    Instalação e configuração do nunjucks. Modularização do header e modal

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
+// Dependência "express" permite criar um servidor web
 const express = require("express"); //Pega a classe express e coloca na variável
 const server = express(); //Inicia o express
+
+// Dependência "nunjucks" é um template engine.
+// Deixa os htmls dinâmicos, perminto a utilização de variáveis, estruturas de repetição e condição
+// Nesse caso será configurado para funcionar junto com o "express"
+const nunjucks = require("nunjucks");
+// Configuração do nunjucks.
+// O primeiro parâmetro é o local onde estão os htmls
+// O segundo, é um objeto que informa propriedades para configuração, como o servidor que está sendo utilizado
+// e o desligamento do cache, para evitar bugs durante o desenvolvimento
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
 
 // Configurar pasta public, como pública.
 // Com isso eu consigo acessar os arquivos pelo servidor na url.
@@ -13,17 +27,23 @@ server.use(express.static("public"));
 server.get("/", (req, res) => {
     // Envia uma reposta
     // res.send("xablau")
+    
     // Envia um arquivo
     // __dirname -> retorna o diretório do arquivo atual
-    res.sendFile(__dirname + "/views/index.html");
+    // res.sendFile(__dirname + "/views/index.html");
+    
+    // Com o nunjucks, a resposta é o html reinderizado.
+    // Ele pega o html dinâmico, reinderiza passando pelo motor, e devolve o html completo.
+    // Como na configuração do nunjucks já foi informado o caminho dos htmls, basta informar o html que será reinderizado e retornado
+    return res.render("index.html");
 });
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html");
+    return res.render("create-point.html");
 });
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html");
+server.get("/search", (req, res) => {
+    return res.render("search-results.html");
 });
 
 // Ligar o servidor
